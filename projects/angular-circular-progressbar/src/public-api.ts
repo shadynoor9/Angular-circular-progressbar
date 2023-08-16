8104f5280544e98761056a1907658ff6b5b58e26
shadynoor9 <shady.noor@kortobaa.com>
2023-08-16 19:31:07 +0300

    update

--- projects/angular-circular-progressbar/src/public-api.ts
+++ projects/angular-circular-progressbar/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of angular-circular-progressbar
  */
 
-// export * from './lib/angular-circular-progressbar.service';
 export * from './lib/angular-circular-progressbar.component';
 export * from './lib/angular-circular-progressbar.module';
